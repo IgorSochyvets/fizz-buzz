d6e7757c199fbbc7263476a12caa5f6ada9af09e
Igor S <isoch@softserveinc.com>
2019-12-26 12:15:11 +0200

    test after helm/tiller upgraded to 2.16.1

--- src/main/java/de/welcz/fizzbuzz/resource/HelloResource.java
+++ src/main/java/de/welcz/fizzbuzz/resource/HelloResource.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloResource {
     @GetMapping
     public String hello() {
-        return "Hello Devops v.10; date: Thu Dec 26 12:05:23 EET 201;  Fizz-Buzz Game Path:  /api/v1/fizz-buzz/numbers/15";
+        return "Hello Devops v.11; deploy time: 12:14:16;  Fizz-Buzz Game Path:  /api/v1/fizz-buzz/numbers/15";
     }
 }
