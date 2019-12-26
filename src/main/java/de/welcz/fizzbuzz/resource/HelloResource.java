d8e62321722bfc462bbbd1f99e2b6d0e61d59292
Igor S <isoch@softserveinc.com>
2019-12-26 13:00:15 +0200

    one more test

--- src/main/java/de/welcz/fizzbuzz/resource/HelloResource.java
+++ src/main/java/de/welcz/fizzbuzz/resource/HelloResource.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloResource {
     @GetMapping
     public String hello() {
-        return "Hello Devops v.14; deploy time: 12:50;  Fizz-Buzz Game Path:  /api/v1/fizz-buzz/numbers/15";
+        return "Hello Devops v.15; deploy time: 13:00;  Fizz-Buzz Game Path:  /api/v1/fizz-buzz/numbers/15";
     }
 }
