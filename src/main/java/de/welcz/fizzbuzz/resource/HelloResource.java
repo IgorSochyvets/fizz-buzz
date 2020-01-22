d0960064130d68dda5c544360d8bbc525ed60fdb
Igor S <isoch@softserveinc.com>
2020-01-22 18:27:36 +0200

    test DEV v.2.0.1

--- src/main/java/de/welcz/fizzbuzz/resource/HelloResource.java
+++ src/main/java/de/welcz/fizzbuzz/resource/HelloResource.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloResource {
     @GetMapping
     public String hello() {
-        return "Hello Devops v.1.0.67;  Fizz-Buzz Game Path:  /api/v1/fizz-buzz/numbers/15";
+        return "Hello Devops v.2.0.1;  Fizz-Buzz Game Path:  /api/v1/fizz-buzz/numbers/15";
     }
 }
