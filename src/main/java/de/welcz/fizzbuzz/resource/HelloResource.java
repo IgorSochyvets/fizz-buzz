8498bdbf8678a2da56470f0d2126fd10da2829a2
Igor S <isoch@softserveinc.com>
2020-02-05 16:34:14 +0200

    test 3

--- src/main/java/de/welcz/fizzbuzz/resource/HelloResource.java
+++ src/main/java/de/welcz/fizzbuzz/resource/HelloResource.java
@@ -10,6 +10,6 @@ public class HelloResource {
     String var1 = "myString";
     @GetMapping
     public String hello() {
-        return "Hello Devops v.2.0.24;  Fizz-Buzz Game Path:  /api/v1/fizz-buzz/numbers/15" + var1;
+        return "Hello Devops v.2.0.25;  Fizz-Buzz Game Path:  /api/v1/fizz-buzz/numbers/15" + var1;
     }
 }
